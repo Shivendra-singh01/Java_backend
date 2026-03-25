5becc219af7981f7e87413b3096ba52869373eb2
Shivendra singh <singhanshul88508@gmail.com>
2026-03-25 23:34:54 +0530

    Mini project 1 : User and Bank account with Validations

---
+++ BankAccount.java
@@ -0,0 +1,60 @@
+public class BankAccount {
+
+    private String accountHolderName;
+    private float balance;
+
+    public void setName(String name) {
+        this.accountHolderName=name;
+    }
+    public void setBalance(float balance){
+        
+        if(balance>=0) {
+            this.balance=balance;
+        }
+        else{
+            System.out.println("Invalid balance");
+        }
+        
+    }
+    public void deposit(float amount) {
+        if(amount>0) {
+            balance+=amount;
+        }
+        else{
+            System.out.println("Enter valid amount");
+        }
+        
+        
+    }
+    public void withdraw(float money) {
+       if (money <= 0) {
+        System.out.println("Enter valid amount to withdraw");
+    }
+    else if (money > balance) {
+        System.out.println("Insufficient balance");
+    }
+    else if (balance - money < 100) {
+        System.out.println("Minimum balance must be maintained");
+    }
+    else {
+        balance -= money;
+    }
+        
+    }
+
+    public void accDetails() {
+        System.out.println("Account Holder Name: "+accountHolderName);
+        System.out.println("Current balance is: "+ balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount account= new BankAccount();
+        account.setName("Anshul singh");
+        account.setBalance(1000);
+        // account.deposit(500);
+        account.accDetails();
+        account.withdraw(0);
+        account.accDetails();
+    }
+}
+
--- User.java
+++ User.java
@@ -1,19 +1,26 @@
 public class User {
-
     String name;
     int age;
+   
+    public User(String name,int age)   {
+        if(age>=0){
+         this.name=name;
+         this.age=age;
+        }
+        else{
+            System.out.println("Invalid age");
+        }
+    }
 
-    void display() {
+     void display() {
         System.out.println("Name: "+ name);
         System.out.println("Age: "+ age);
         }
-    
+
     public static void main(String[] args) {
-        User u = new User();
-        u.name="Anshul singh";
-        u.age=22;
-        u.display();
-    
+        User user = new User("Anshul",22);
+        user.display();
+        
     }
-    }
+}
 
