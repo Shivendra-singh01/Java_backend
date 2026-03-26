1bfe5b3e9764da9af53aafa2d5540ac8408f260c
Shivendra singh <singhanshul88508@gmail.com>
2026-03-26 19:30:43 +0530

    Mini project 2: Multiple user bankaccounts

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BankAccount {
 
     private String accountHolderName;
@@ -48,13 +50,31 @@ public class BankAccount {
     }
 
     public static void main(String[] args) {
-        BankAccount account= new BankAccount();
-        account.setName("Anshul singh");
-        account.setBalance(1000);
-        // account.deposit(500);
-        account.accDetails();
-        account.withdraw(0);
-        account.accDetails();
+        BankAccount account_01= new BankAccount();
+        account_01.setName("Anshul singh");
+        account_01.setBalance(1000);
+        account_01.deposit(500);
+
+        BankAccount account_02= new BankAccount();
+        account_02.setName("Sidd");
+        account_02.setBalance(10000);
+        
+        BankAccount account_03= new BankAccount();
+        account_03.setName("Nitin");
+        account_03.setBalance(500);
+
+        ArrayList<BankAccount> accounts= new ArrayList<>();
+        accounts.add(account_01);
+        accounts.add(account_02);
+        accounts.add(account_03);
+        
+        accounts.get(0).withdraw(100);
+        accounts.get(1).deposit(100);
+        accounts.get(2).deposit(300);
+
+        for(BankAccount u: accounts) {
+            u.accDetails();
+        }
     }
 }
 
