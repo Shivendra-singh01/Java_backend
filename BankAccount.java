70d296b13a6f18abefada12cd28d044327cbc50e
Shivendra singh <singhanshul88508@gmail.com>
2026-03-27 17:51:01 +0530

    Mini project 3: Menu-driven Bank System with validations

--- BankAccount.java
+++ BankAccount.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BankAccount {
 
@@ -53,7 +54,6 @@ public class BankAccount {
         BankAccount account_01= new BankAccount();
         account_01.setName("Anshul singh");
         account_01.setBalance(1000);
-        account_01.deposit(500);
 
         BankAccount account_02= new BankAccount();
         account_02.setName("Sidd");
@@ -68,13 +68,74 @@ public class BankAccount {
         accounts.add(account_02);
         accounts.add(account_03);
         
-        accounts.get(0).withdraw(100);
-        accounts.get(1).deposit(100);
-        accounts.get(2).deposit(300);
+        // accounts.get(0).withdraw(100);
+        // accounts.get(1).deposit(100);
+        // accounts.get(2).deposit(300);
+        // for(BankAccount u: accounts) {
+        //     u.accDetails();
+        // }
+        
+        Scanner sc= new Scanner(System.in);
+        while(true) {
+            System.out.println("Enter 1: Deposit");
+            System.out.println("Enter 2: Withdraw");
+            System.out.println("Enter 3: View All Accounts");
+            System.out.println("Enter 4: Exit");
+            System.out.print("Please give input: ");
+            int i= sc.nextInt();
+            switch(i) {
+                case 1:
+                    System.out.println("Enter your Account index: ");
+                    int j=sc.nextInt();
+                    if(j>=0 && j<accounts.size()) {
+                        System.out.println("Your Accounts Details are:");
+                        accounts.get(j).accDetails();
+                        System.out.println("Please Verify and enter the Amount to Deposit: ");
+                        float newDepositAmount = sc.nextFloat();
+                        accounts.get(j).deposit(newDepositAmount);
+                        System.out.println("Your Updated Balance is:");
+                        accounts.get(j).accDetails();
+                    }
+                    else{
+                        System.out.println("Invalid Index!");
+                    }
+                    break;
+                
+                case 2:
+                    System.out.println("Enter your Account index: ");
+                    int k=sc.nextInt();
+                    if(k>=0 && k<accounts.size()) {
+                        System.out.println("Your Accounts details are:");
+                        accounts.get(k).accDetails();
+                        System.out.println("Please Verify and Enter the Amount to Withdraw: ");
+                        float newWithdrawAmount = sc.nextFloat();
+                        accounts.get(k).withdraw(newWithdrawAmount);
+                        System.out.println("Your Updated Balance is:");
+                        accounts.get(k).accDetails();
+                    }
+                    else{
+                        System.out.println("Invalid Index!");
+                    }
+                    break;
 
-        for(BankAccount u: accounts) {
-            u.accDetails();
+                case 3:
+                    for(BankAccount u: accounts) {
+                    u.accDetails();
+                }
+                break;
+
+                case 4:
+                   System.out.println("Exiting...");
+                   sc.close();
+                   return; 
+
+                default:
+                    System.out.println("Choose a Valid Option!");
+            }    
+        
         }
     }
+
+
 }
 
